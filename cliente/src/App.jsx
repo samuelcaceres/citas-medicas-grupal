f2ef0080d5fce2dd5fddc241a723d9800c20e1ec
Samuel Cáceres <144251952+samuelcaceres@users.noreply.github.com>
2024-03-06 21:04:02 -0300

    Update App.jsx

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './Componentes/Home/Home';
 import EditarCita from './Componentes/Edditarcitas/EditarCita';
 import RegisterForm from './Componentes/Login/RegisterForm'; 
 import LoginForm from './Componentes/Login/LoginForm'; 
-import Nosotros from './Componentes/Nosotros/Nosotros'; // Importa el componente Nosotros
+import Nosotros from './Componentes/Nosotros/Nosotros'; 
 import Profesionales from './Componentes/Nosotros/Profesionales';
 import Especialidades from './Componentes/Nosotros/Especialidades';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -21,7 +21,6 @@ function App() {
       <Route path="/editar" element={<VisualizarCitas />} /> 
       <Route path="/editar/:id" element={<EditarCita />} />
       <Route path="/borrar/:id" />
-      {/* Agrega la ruta para el componente Nosotros */}
       <Route path="/nosotros" element={<Nosotros />} />
       <Route path="/profesionales" element={<Profesionales />} />
       <Route path="/especialidades" element={<Especialidades />} />
